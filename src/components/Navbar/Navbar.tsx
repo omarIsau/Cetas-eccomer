e6ba3cf48412446bf1a105ee845d6ea110aa6e68
felipevalbuena <felipedev1309@gmail.com>
2024-04-03 14:04:46 -0500

    commit de prueva clouflere

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,17 +4,10 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4">
-        <a
-          href="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src="/logo.webp"
-            className="h-8"
-            alt="Setas loog"
-          />
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="/logo.webp" className="h-8" alt="Setas loog" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Setitas.com
+            pruevaClouflere.com
           </span>
         </a>
         <NavToch />
